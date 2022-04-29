e44e7838a2b500e7d7ebdaa704476d16a859c8b0
aarkan <johanwiren1@gmail.com>
2022-04-29 14:39:24 +0200

    Update demo example

---
+++ frontend/serviceWorker.js
@@ -0,0 +1,22 @@
+// A Service Workers lifecycle:
+// 1. Service Worker gets registered and 'install'-event fires
+// 2. The second time the user enters the page the 'activate'-event fires
+// 3. The third time the user enters the page the 'fetch'-event starts working
+
+// These steps are to prevent weird cache behavior.
+// Steps gets reset when the Service Worker file changes.
+// We can bypass the reload requirements by using 'skipWaiting()' and 'clients.claim()' like below
+
+// listener mostly used for pre-caching assets
+addEventListener("install", () => {
+    console.log("[PWA] installing");
+    // start 'activate'-event immediately
+    skipWaiting();
+});
+
+// listener mostly used for cleaning up old cache
+addEventListener("activate", () => {
+    console.log("[PWA] activating");
+    // start 'fetch'-event immediately
+    clients.claim();
+});
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,10 @@
 // init router
 import "./router.js";
+// init Progressive Web App features
+import "./initPWA.js";
 
 const backButton = document.querySelector("#back-button");
 const upButton = document.querySelector("#up-button");
-const installButton = document.querySelector("#install-button");
 
 backButton.addEventListener("click", () => {
     history.back();
@@ -18,10 +19,4 @@ window.addEventListener("scroll", () => {
 upButton.addEventListener("click", () => {
     if (window.scrollY < 300) return;
     window.scrollTo(0, 0);
-});
-
-installButton.addEventListener("click", async () => {
-    console.log("Install PWA button clicked");
-
-    installButton.classList.add("invisible");
 });
